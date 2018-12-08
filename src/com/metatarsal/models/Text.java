f7155ea8039faee7606e2be5254669074b5d7249
JeannieFallon <jeanniefallon@gmail.com>
2018-12-08 17:25:37 -0500

    text TO getters & setters

--- src/com/metatarsal/models/Text.java
+++ src/com/metatarsal/models/Text.java
@@ -1,4 +1,40 @@
 package com.metatarsal.models;
 
 public class Text {
+
+    private String plainTxt;
+    private String cipherTxt;
+    private int txtLength;
+
+    public Text() {
+
+    }
+
+    public Text(String plainTxt) {
+        this.plainTxt = plainTxt;
+    }
+
+    public String getPlainTxt() {
+        return plainTxt;
+    }
+
+    public void setPlainTxt(String plainTxt) {
+        this.plainTxt = plainTxt;
+    }
+
+    public String getCipherTxt() {
+        return cipherTxt;
+    }
+
+    public void setCipherTxt(String cipherTxt) {
+        this.cipherTxt = cipherTxt;
+    }
+
+    public int getTxtLength() {
+        return txtLength;
+    }
+
+    public void setTxtLength() {
+        this.txtLength = plainTxt.length();
+    }
 }
