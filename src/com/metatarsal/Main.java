52bdc08847a7bdf3f88e016fd5d13253d5f028e3
JeannieFallon <jeanniefallon@gmail.com>
2018-12-08 18:07:00 -0500

    scanner in last commit, stub out caesar in this commit

--- src/com/metatarsal/Main.java
+++ src/com/metatarsal/Main.java
@@ -1,19 +1,24 @@
 package com.metatarsal;
+import com.metatarsal.controllers.CaesarController;
 import com.metatarsal.models.Text;
 import java.util.Scanner;
 
 public class Main {
 
+
     public static void main(String[] args) {
 
+        CaesarController caesarController = new CaesarController();
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter the text to be enciphered:\n");
 
         String input = scanner.nextLine();
         Text text = new Text(input);
+        text = caesarController.goCaesarCipher(text);
 
         System.out.println("Plain text: " + text.getPlainTxt());
+        System.out.println("Cipher text: " + text.getCipherTxt());
         System.out.println("Length of text: " + text.getTxtLength());
     }
 }
--- src/com/metatarsal/controllers/CaesarController.java
+++ src/com/metatarsal/controllers/CaesarController.java
@@ -1,6 +1,14 @@
 package com.metatarsal.controllers;
 
+import com.metatarsal.models.Text;
+
 public class CaesarController {
 
+    public Text goCaesarCipher(Text text) {
 
+        String cipherTxt = "This is the test cipher text";
+        text.setCipherTxt(cipherTxt);
+
+        return text;
+    }
 }
