bd3e861c9bebeb68d108c85414b6e5bcd3584bfc
JeannieFallon <jeanniefallon@gmail.com>
2018-12-09 13:06:34 -0500

    caesar improvements

--- src/com/metatarsal/controllers/CaesarController.java
+++ src/com/metatarsal/controllers/CaesarController.java
@@ -8,55 +8,48 @@ public class CaesarController {
 * 09 Dec 18 - Working draft
 * */
     public Text goCaesarCipher(Text text) {
-        String cipherTxt = "";
+        StringBuilder sb = new StringBuilder(); //use StringBuffer instead?
         int txtLen = text.getTxtLength();
         int shiftVal = text.getShiftVal();
-        char[] plainArr = text.getPlainTxt().toCharArray();
-        char plainChar;
+        char[] charArr = text.getPlainTxt().toCharArray();
 
         for (int i = 0; i < txtLen; i++) {
-            plainChar = plainArr[i];
-            cipherTxt += shiftChar(plainChar, shiftVal);
+            sb.append(shiftChar(charArr[i], shiftVal));
         }
 
-        text.setCipherTxt(cipherTxt);
+        text.setCipherTxt(sb.toString());
 
         return text;
     }
 
-    public String shiftChar(char charToShift, int shiftVal) {
-        String cipherChar;
+    public char shiftChar(char charToShift, int shiftVal) {
         int asciiVal = (int)charToShift;
-        int alphaDiffVal = 0;
+        int asciiDiffVal = 0;
         boolean isShift = false;
 
         // only shift upper or lower case letters in text
         if (asciiVal >= 97 && asciiVal <= 122) {
             isShift = true;
-            alphaDiffVal = 97;
+            asciiDiffVal = 97;
         } else if (asciiVal >= 65 && asciiVal <= 90) {
             isShift = true;
-            alphaDiffVal = 65;
+            asciiDiffVal = 65;
         }
 
         if (isShift) {
-            asciiVal = getCipherAscii(asciiVal, alphaDiffVal, shiftVal);
+            asciiVal = getCipherAscii(asciiVal, asciiDiffVal, shiftVal);
         }
 
-        //TODO make more efficient by returning int & converting up one level
-        // get rid of duplicate String initialization
-        cipherChar = Character.toString((char)asciiVal);
-
-        return cipherChar;
+        return (char)asciiVal;
     }
 
-    public int getCipherAscii(int asciiVal, int alphaDiffVal, int shiftVal) {
+    public int getCipherAscii(int asciiVal, int asciiDiffVal, int shiftVal) {
         // get index of letter, assuming standard alphabet a - z with indices 0 - 25
-        int alphaIdx = asciiVal - alphaDiffVal;
-        // wrap rotation around standard alphabet
+        int alphaIdx = asciiVal - asciiDiffVal;
+        // wrap rotation around standard alphabet indices
         alphaIdx = getRotation(alphaIdx, shiftVal);
         // return to original ASCII range
-        return alphaIdx + alphaDiffVal ;
+        return alphaIdx + asciiDiffVal;
     }
 
     public int getRotation(int alphaIdx, int shiftVal) {
