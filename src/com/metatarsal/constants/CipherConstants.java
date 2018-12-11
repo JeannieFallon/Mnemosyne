94ac841079b9e07f06ff67a8577e13cd3d69c229
JeannieFallon <jeanniefallon@gmail.com>
2018-12-11 11:37:08 -0500

    replace caesar hardcoded range values with constants

--- src/com/metatarsal/constants/CipherConstants.java
+++ src/com/metatarsal/constants/CipherConstants.java
@@ -2,5 +2,18 @@ package com.metatarsal.constants;
 
 public class CipherConstants {
 
+    /*
+    * floor of upper or lower case range will also serve as
+    * difference between ASCII value and alphabet index
+    * */
 
+    private CipherConstants() {
+
+    }
+
+    public static final int ENG_ALPHA_LEN = 26;
+    public static final int ENG_UPPER_FLOOR = 65;
+    public static final int ENG_UPPER_CEILING = 90;
+    public static final int ENG_LOWER_FLOOR = 97;
+    public static final int ENG_LOWER_CEILING = 122;
 }
--- src/com/metatarsal/controllers/CaesarController.java
+++ src/com/metatarsal/controllers/CaesarController.java
@@ -1,12 +1,16 @@
 package com.metatarsal.controllers;
+import com.metatarsal.constants.CipherConstants;
 import com.metatarsal.models.Text;
+
+import javax.crypto.Cipher;
 
 public class CaesarController {
 
 /*
 * Run Caesar cipher on input text string using input integer as shift value.
-* 09 Dec 18 - Working draft
+* 11 Dec 18 - Added constants
 * */
+
     public Text goCaesarCipher(Text text) {
         StringBuilder sb = new StringBuilder(); //use StringBuffer instead?
         int txtLen = text.getTxtLength();
@@ -24,35 +28,27 @@ public class CaesarController {
 
     public char shiftChar(char charToShift, int shiftVal) {
         int asciiVal = (int)charToShift;
-        int asciiDiffVal = 0;
-        boolean isShift = false;
 
         // only shift upper or lower case letters in text
-        if (asciiVal >= 97 && asciiVal <= 122) {
-            isShift = true;
-            asciiDiffVal = 97;
-        } else if (asciiVal >= 65 && asciiVal <= 90) {
-            isShift = true;
-            asciiDiffVal = 65;
-        }
-
-        if (isShift) {
-            asciiVal = getCipherAscii(asciiVal, asciiDiffVal, shiftVal);
+        if (asciiVal >= CipherConstants.ENG_LOWER_FLOOR && asciiVal <= CipherConstants.ENG_LOWER_CEILING) {
+            asciiVal = getCipherAscii(CipherConstants.ENG_LOWER_FLOOR, asciiVal, shiftVal);
+        } else if (asciiVal >= CipherConstants.ENG_UPPER_FLOOR && asciiVal <= CipherConstants.ENG_UPPER_CEILING) {
+            asciiVal = getCipherAscii(CipherConstants.ENG_UPPER_FLOOR, asciiVal, shiftVal);
         }
 
         return (char)asciiVal;
     }
 
-    public int getCipherAscii(int asciiVal, int asciiDiffVal, int shiftVal) {
-        // get index of letter, assuming standard alphabet a - z with indices 0 - 25
+    public int getCipherAscii(int asciiDiffVal, int asciiVal, int shiftVal) {
+        // map ASCII value onto index of letter in alphabet
         int alphaIdx = asciiVal - asciiDiffVal;
-        // wrap rotation around standard alphabet indices
+        // wrap rotation around alphabet indices
         alphaIdx = getRotation(alphaIdx, shiftVal);
         // return to original ASCII range
         return alphaIdx + asciiDiffVal;
     }
 
     public int getRotation(int alphaIdx, int shiftVal) {
-        return (alphaIdx + shiftVal) % 26;
+        return (alphaIdx + shiftVal) % CipherConstants.ENG_ALPHA_LEN;
     }
 }
